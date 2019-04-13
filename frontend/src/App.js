104b16702c80779706105c9bc7bc5a3ecc94881c
Wesley Souza <wesleyadriann@gmail.com>
2019-04-13 19:27:05 -0300

    Torna a navbar funcional

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,8 +10,11 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      search: '',
       pokemons : []
+      
     }
+    this.handleChange = this.handleChange.bind(this)
   }
 
   
@@ -20,29 +23,40 @@ class App extends Component {
       .then(resp => {
         this.setState({
           pokemons : resp.data,
-          search: ''
         })
       })
   }
   
   handleChange(e) {
-    console.log(e.target.value)
-    // this.setState({
-    //   [e.target.name] : e.target.value
-    // });
+    this.setState({
+      [e.target.name] : e.target.value
+    })
   }
   
   render() {
+    let count = 0;
     return (
       <div>
         <Navbar handleChange={this.handleChange}/>
         <div className="container pt-5 mt-5 ">
           <div className="row justify-content-center">
             {this.state.pokemons.map(poke => {
-              console.log(poke)
-              return(
-                <Pokemon id={poke.id} nome={poke.nome} imagem={poke.imagem} tipo0={poke.tipo0} tipo1={poke.tipo1}/>
-              )
+              if((this.state.search === '') ||
+                (poke.nome.includes(this.state.search.toLocaleLowerCase()))) {
+                  return(
+                    <Pokemon id={poke.id} nome={poke.nome} imagem={poke.imagem} tipo0={poke.tipo0} tipo1={poke.tipo1}/>
+                  )
+              } else {
+                count++;
+                console.log(count)
+              }
+              if(count === 151) {
+                return (
+                  <div align="center" className="col mt-5" key={count}>
+                    <p className="h1">No results to show </p>
+                  </div>
+                )
+              }
             })}
           </div>
         </div>

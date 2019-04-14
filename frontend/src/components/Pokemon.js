c6fec8e6cf4a9ae1f03739a027537e8ada1d71d2
Wesley Souza <wesleyadriann@gmail.com>
2019-04-14 15:55:53 -0300

    Faz o upload do banco de dados e adiciona as badges

--- frontend/src/components/Pokemon.js
+++ frontend/src/components/Pokemon.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import '../css/pokemon.min.css'
 
 const Pokemon = props => {
+    let tipo1;
+    if (props.tipo1 != '') {
+        tipo1 = <span className={`badge badge-${props.tipo1}`}>{props.tipo1}</span>
+    }
     return (
         <div className="col-sm-3 m-1 p-1 border rounded" key={props.id}>
             <div className="media" >
@@ -13,7 +18,10 @@ const Pokemon = props => {
                 <div className="media-body" onClick={() => {props.handleChain(props.evochain)}}>
                     <h5 className="mt-0">{props.nome}</h5>
                     PokeID: {props.id}<br/>
-                    <h6>{props.tipo1} - {props.tipo0}</h6>
+                    <h6>   
+                        <span className={`badge badge-${props.tipo0}`}>{props.tipo0}</span> &nbsp;
+                        {tipo1} &nbsp;
+                    </h6>
                 </div>
                 <span className="badge pt-0 pr-1 badge-danger" style={{verticalAlign: 'text-top'}}>×</span>                   
             </div>
